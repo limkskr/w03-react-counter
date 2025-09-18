5ce1dbc4c4592a109d781c3ce95fe5cdd29f836d
limkskr <ksks3164@gmail.com>
2025-09-18 14:35:25 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {useState} from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1>카운터 {count} </h1>
+      <button 
+        onClick={ () => {
+          setCount((prev) => prev + 1)
+          }
+        }
+      >증가
+      </button>
+      <button onClick = { () => setCount(0)  }>초기화</button>
+    </>
+  )
+}
+
+export default App
